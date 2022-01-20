e9e72a2f29fb4222224ed97bba396b8578ef1c36
Rohit Thapar <thaprt206@gmail.com>
2022-01-20 09:26:46 +0530

    new files

--- DSA/DP/knapsack01dp.cpp
+++ DSA/DP/knapsack01dp.cpp
@@ -7,7 +7,7 @@ int knapsack(int wt[],int val[],int w, int n){
     for(int i=0;i<n+1;i++){
         for(int j=0;j<w+1;j++){
             if(i == 0 || j == 0){
-                dp[i][j] == 0;
+                dp[i][j] = 0;
             }
         }
     }
