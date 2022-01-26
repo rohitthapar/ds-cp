0cbeb25a6aa6bc0a1365a3df176a479a07e7e023
Rohit Thapar <thaprt206@gmail.com>
2022-01-26 11:36:51 +0530

    file done -- changes made

--- DSA/DP/UnBoundedKnapSack/tempCodeRunnerFile.cpp
+++ DSA/DP/UnBoundedKnapSack/tempCodeRunnerFile.cpp
@@ -1,9 +1,28 @@
-for(int i=1;i<n+1;i++){
-        for(int j=1;j<s+1;j++){
-            if(arr[i-1]<=j){
-                dp[i][j] = min(1+dp[i][j-arr[i-1]],dp[i-1][j]);
-            }
-            else dp[i][j] =  dp[i-1][j];
-        }
+#include<iostream>
+using namespace std;
+int binarySearch_rec(int arr[],int l,int r,int x){
+    if(r>=l){
+        int mid = l+(r-l)/2;
+    if(arr[mid] == x){
+        return mid;
     }
-    return dp[n][s];
+    if(arr[mid] > mid){
+        return binarySearch_rec(arr,l,mid-1,x);
+    }
+    else return binarySearch_rec(arr,mid+1,r,x);
+    }
+    return -1;
+
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int x;
+    cin>>x;
+    cout<<binarySearch_rec(arr,0,n-1,x)<<endl;
+    return 0;
+}
--- DSA/mergesort.cpp
+++ DSA/mergesort.cpp
@@ -1,9 +1,58 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-//using recursion
-
-
-int main(){
-
-    return 0;
+void merge(int *,int, int , int );
+void merge_sort(int *arr, int low, int high)
+{
+    int mid;
+    if (low < high){
+        mid=low+(high-low)/2;
+        merge_sort(arr,low,mid);
+        merge_sort(arr,mid+1,high);
+        merge(arr,low,high,mid);
+    }
+}
+void merge(int *arr, int low, int high, int mid)
+{
+    int i, j, k, c[50];
+    i = low;
+    k = low;
+    j = mid + 1;
+    while (i <= mid && j <= high) {
+        if (arr[i] < arr[j]) {
+            c[k] = arr[i];
+            k++;
+            i++;
+        }
+        else  {
+            c[k] = arr[j];
+            k++;
+            j++;
+        }
+    }
+    while (i <= mid) {
+        c[k] = arr[i];
+        k++;
+        i++;
+    }
+    while (j <= high) {
+        c[k] = arr[j];
+        k++;
+        j++;
+    }
+    for (i = low; i < k; i++)  {
+        arr[i] = c[i];
+    }
+}
+int main()
+{
+    int arr[30], num;
+    cin>>num;
+    for (int i = 0; i < num; i++) { cin>>arr[i];
+    }
+    merge_sort(arr, 0, num-1);
+    cout<<"Sorted array\n";
+    for (int i = 0; i < num; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
 }
