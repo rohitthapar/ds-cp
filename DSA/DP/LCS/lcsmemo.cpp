e51a8b036824c40b0d5cf499724a9129a85b702d
Rohit Thapar <thaprt206@gmail.com>
2022-01-26 19:53:00 +0530

    file done -- changes made

--- DSA/DP/LCS/lcsmemo.cpp
+++ DSA/DP/LCS/lcsmemo.cpp
@@ -1,16 +1,22 @@
 #include<iostream>
 #include<string.h>
+#include<cstring>
 using namespace std;
+int dp[102][1002];
 int countLCS(string x,string y,int n,int m){
     if(n == 0 || m == 0){
         return 0;
     }
+    if(dp[n][m] != -1){
+        return dp[n][m];
+    }
     if(x[n-1] == y[m-1]){
         return 1+countLCS(x,y,n-1,m-1);
     }
-    else return max(countLCS(x,y,n-1,m),countLCS(x,y,n,m-1));
+    else return dp[n][m] = max(countLCS(x,y,n-1,m),countLCS(x,y,n,m-1));
 }
 int main(){
+    memset(dp,-1,sizeof(dp));
     string x;
     string y;
     cin>>x>>y;
