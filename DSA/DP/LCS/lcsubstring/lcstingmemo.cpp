7c680e57877cc6f0494280a80506429edfb36c3d
Rohit Thapar <thaprt206@gmail.com>
2022-01-27 19:34:45 +0530

    New File

--- DSA/DP/LCS/lcsubstring/lcstingmemo.cpp
+++ DSA/DP/LCS/lcsubstring/lcstingmemo.cpp
@@ -1,31 +1,69 @@
-//giving wrong answer
+// //giving wrong answer
+// #include<iostream>
+// #include<string.h>
+// #include<cstring>
+// using namespace std;
+// int dp[102][1002];
+// int countLcstring(string x,string y,int n,int m,int count){
+//     if(n == 0 || m == 0){
+//         return count;
+//     }
+//     if(dp[n][m]!=-1){
+//         return dp[n][m];
+//     }
+//     if(x[n-1] == y[m-1]){
+//         return  dp[n][m] = countLcstring(x,y,n-1,m-1,count+1);
+//     }
+//     else return  dp[n][m] = max(count,max(countLcstring(x,y,n-1,m,0),countLcstring(x,y,n,m-1,0)));
+// }
+// int main(){
+//     memset(dp,-1,sizeof(dp));
+//     string x;
+//     string y;
+//     cin>>x>>y;
+//     int n = x.length();
+//     int m = y.length();
+//     int count;
+//     cout<<countLcstring(x,y,n,m,0)<<endl;
+//     return 0;
+// }
+// //abcdgh abedfhr
+// //4
+
 #include<iostream>
 #include<string.h>
 #include<cstring>
 using namespace std;
 int dp[102][1002];
-int countLcstring(string x,string y,int n,int m,int count){
+int ans;
+int countLcstring(string x,string y,int n,int m){
     if(n == 0 || m == 0){
-        return count;
+        return 0;
     }
     if(dp[n][m]!=-1){
         return dp[n][m];
     }
+    countLcstring(x,y,n-1,m);
+    countLcstring(x,y,n,m-1);
+
     if(x[n-1] == y[m-1]){
-        return  dp[n][m] = countLcstring(x,y,n-1,m-1,count+1);
+        dp[n][m] = countLcstring(x,y,n-1,m-1) + 1;
+        ans = max(ans, dp[n][m]);
+        return dp[n][m];
     }
-    else return  dp[n][m] = max(count,max(countLcstring(x,y,n-1,m,0),countLcstring(x,y,n,m-1,0)));
+    
+    return  dp[n][m] = 0;
 }
 int main(){
     memset(dp,-1,sizeof(dp));
+    ans = 0;
     string x;
     string y;
     cin>>x>>y;
     int n = x.length();
     int m = y.length();
     int count;
-    cout<<countLcstring(x,y,n,m,0)<<endl;
+    countLcstring(x,y,n,m);
+    cout<<ans<<endl;
     return 0;
 }
-//abcdgh abedfhr
-//4
