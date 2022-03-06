414a0428582c0dde3828c347ade4d6493f92b98d
Rohit Thapar <thaprt206@gmail.com>
2022-03-06 14:58:07 +0530

    changes

--- DSA/hashmaps/hashmaps.cpp
+++ DSA/hashmaps/hashmaps.cpp
@@ -9,14 +9,23 @@ using namespace std;
 
 
 int main(){
-    map<string,int> marksMap;
-    marksMap["Rohit"] = 10;
-    marksMap["ROhan"] = 100;
-    marksMap["Rahul"] = 1000;
-    map<string,int> :: iterator it;
-    for(it=marksMap.begin();it!=marksMap.end();it++){
-        cout<<(*it).first<<" "<<(*it).second<<"\n";
+        // map<string,int> marksMap;
+        // marksMap["Rohit"] = 10;
+        // marksMap["ROhan"] = 100;
+        // marksMap["Rahul"] = 1000;
+        // map<string,int> :: iterator it;
+        // for(it=marksMap.begin();it!=marksMap.end();it++){
+        //     cout<<(*it).first<<" "<<(*it).second<<"\n";
+        // }
+        // cout<<marksMap.empty();
+
+    unordered_map<int,char> mp;
+    mp = {{1,'a'},{2,'b'}};
+    // cout<<mp[1];
+    unordered_map<int,char> :: iterator it;
+    for(it=mp.begin();it!=mp.end();it++){
+        cout<<it->first<<" "<<it->second<<endl;
     }
-    cout<<marksMap.empty();
+
     return 0;
 }
