acee7f89a667ace5e8f43e02f6ed54a2737e1d27
Rohit Thapar <thaprt206@gmail.com>
2022-03-06 14:48:08 +0530

    changes

--- DSA/hashmaps/hashmaps.cpp
+++ DSA/hashmaps/hashmaps.cpp
@@ -1,12 +1,22 @@
 #include<iostream>
 #include<list>
 #include<cstring>
+#include<map>
+#include<unordered_map>
 using namespace std;
 
 //hashtable to implement phonebook
 
 
 int main(){
-
+    map<string,int> marksMap;
+    marksMap["Rohit"] = 10;
+    marksMap["ROhan"] = 100;
+    marksMap["Rahul"] = 1000;
+    map<string,int> :: iterator it;
+    for(it=marksMap.begin();it!=marksMap.end();it++){
+        cout<<(*it).first<<" "<<(*it).second<<"\n";
+    }
+    cout<<marksMap.empty();
     return 0;
 }
