7da3543b97da776f3b851f33d8a44da0a4bc0e68
Rohit Thapar <thaprt206@gmail.com>
2022-03-26 15:57:25 +0530

    study

--- DSA/DP/Knapsack/01knapsack.cpp
+++ DSA/DP/Knapsack/01knapsack.cpp
@@ -1,16 +1,22 @@
 #include<iostream>
 #include<vector>
+// #include<cstring>
 using namespace std;
+// int dp[102][1002];
 int knapsack(int wt[],int val[],int w, int n){
   if(n == 0 || w == 0){
       return 0;
   }
+//   if(dp[n][w]!=-1){
+//       return dp[n][w];
+//   }
   if(wt[n-1]<=w){
-      return max(val[n-1] + knapsack(wt,val,w-val[n-1],n-1),knapsack(wt,val,w,n-1));
+      return /* dp[n][w] = */ max(val[n-1] + knapsack(wt,val,w-val[n-1],n-1),knapsack(wt,val,w,n-1));
   }
   else return knapsack(wt,val,w,n-1);
 }
 int main(){
+    // memset(dp,-1,sizeof(dp));
     int n,w;
     cin>>n>>w;
     int wt[n],val[n];
