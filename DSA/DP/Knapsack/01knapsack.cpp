c686b89e010230c80e30e0710987e4e44f59e25a
Rohit Thapar <thaprt206@gmail.com>
2022-03-26 15:53:32 +0530

    changes

--- DSA/DP/Knapsack/01knapsack.cpp
+++ DSA/DP/Knapsack/01knapsack.cpp
@@ -2,13 +2,13 @@
 #include<vector>
 using namespace std;
 int knapsack(int wt[],int val[],int w, int n){
-    if(n==0 || w==0){
-        return 0;
-    }
-    if(wt[n-1]<=w){
-        return max(val[n-1]+knapsack(wt,val,w-wt[n-1],n-1),knapsack(wt,val,w,n-1));
-    }
-    else return knapsack(wt,val,w,n-1);
+  if(n == 0 || w == 0){
+      return 0;
+  }
+  if(wt[n-1]<=w){
+      return max(val[n-1] + knapsack(wt,val,w-val[n-1],n-1),knapsack(wt,val,w,n-1));
+  }
+  else return knapsack(wt,val,w,n-1);
 }
 int main(){
     int n,w;
@@ -19,6 +19,5 @@ int main(){
 
     }
     cout<<knapsack(wt,val,w,n)<<endl;    
-
     return 0;
 }
