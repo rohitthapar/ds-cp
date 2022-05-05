8949bd1968df9440a32b5dc385f126a4b1af6f07
Rohit Thapar <thaprt206@gmail.com>
2022-05-05 09:36:39 +0530

    changes

--- DSA/DP/chef.cpp
+++ DSA/DP/chef.cpp
@@ -1,11 +1,29 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
 struct chef{
     int start,finish,compartment;
 };
 bool compare(chef c1, chef c2){
-    return (c1.finish < c2.finish);
+    if(c1.compartment == c2.compartment)return (c1.finish < c2.finish);
+    return (c1.compartment < c2.compartment);
+}
+int maxComp(chef res[], int n){
+    sort(res,res+n,compare);
+    int i=0;
+     int s = (n>0) ? 1:0;
+    for(int j=1;j<n;j++){
+        if(res[j].compartment!=res[i].compartment){
+            s++;
+            i=j;
+        }
+        else if(res[j].start >= res[i].finish){
+            s++;
+            i=j;
+        }
+    }
+    return s;
 }
 int main(){
     int t;
@@ -21,6 +39,7 @@ int main(){
         //     cout<<res[i].start<<" "<<res[i].finish<<" "<<res[i].compartment;
         //     cout<<endl;
         // }
+        cout<<maxComp(res,n)<<endl;
     }
     return 0;
 }
