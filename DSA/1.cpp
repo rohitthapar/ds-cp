cccffa6e1857b1b28dc936bf4f279b32d9f2eb58
Rohit Thapar <thaprt206@gmail.com>
2022-07-19 17:41:37 +0530

    txt file

---
+++ DSA/1.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+int calc(string s) {
+    int n = s.size();
+    vector<string>st = {"zero", "two", "four", "six", "eight", "one", "three", "five", "seven", "nine"};
+    vector<int>v = {0, 2, 4, 6, 8, 1, 3, 5, 7, 9};
+    string ans = "";
+    unordered_map<int, int>cnt;
+    for (int i = 0; i < n; i++) {
+        cnt[s[i] - 'a']++;
+    }
+    for (int i = 0; i < st.size(); i++) {
+        string cur = st[i];
+        unordered_map<int, int>f;
+        for (int j = 0; j < cur.size(); j++) {
+            f[cur[j] - 'a']++;
+        }
+        while (1) {
+            bool flg = true;
+            for (auto &x : f) {
+                if (cnt[x.first] < f[x.first]) {
+                    flg = false; break;
+                }
+            }
+            if (flg == false) {
+                break;
+            }
+            for (auto &x : f) {
+                cnt[x.first] -= f[x.first];
+            }
+            char ch = v[i] + '0';
+            ans += ch;
+        }
+    }
+    sort(begin(ans), end(ans));
+    int sum = 0;
+    for (int i = 0; i < ans.size(); i++) {
+        int d = ans[i] - '0';
+        sum += d;
+    }
+    return sum;
+}
+int main()
+{
+    string s;
+    cin>>s;
+    cout<<calc(s)<<endl;
+    return 0;
+}
