180e9d2fb8d7e245ac39528dcd587baee76c1f6a
Rohit Thapar <thaprt206@gmail.com>
2022-08-15 11:50:45 +0530

    Add Files

--- DSA/Graphs/adjacencymatrix.cpp
+++ DSA/Graphs/adjacencymatrix.cpp
@@ -7,7 +7,7 @@ void adjmatrix(){
   int n,e;    // n -> nodes, m -> edges
     cin>>n>>e;
     vector<vector<int>> adj(n+1,vector<int>(n+1,0));
-    for(int i=0;i<m;i++){
+    for(int i=0;i<e;i++){
         int u,v;
         cin>>u>>v;
         adj[u][v] = 1;
@@ -15,7 +15,42 @@ void adjmatrix(){
     }
     // we can only use adjacency matrix when the size of input is small
 }
+void adjlist(){
+    int n,e;
+    cin>>n>>e;
+    vector<int> adj[n+1];
+    for(int i=0;i<e;i++){
+        int u,v;
+        cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+}
+
+void adjlistDirectedGraph(){
+    int n,e;
+    cin>>n>>e;
+    vector<int> adj[n+1];
+    for(int i=0;i<e;i++){
+        int u,v;
+        cin>>u>>v;
+        adj[u].push_back(v);
+    }
+}
+
+void adjListDirectegWeightedGraph(){
+    int n,e;
+    cin>>n>>e;
+    vector<pair<int,int>> adj[n+!];
+    for(int i=0;i<e;i++){
+        int u,v,w;
+        cin>>u>>v>>w;
+        adj[u].push_back({v,w});
+    }
+}
 int main()
 {
+    adjmatrix();
+    adjlist();
     return 0;
 }
