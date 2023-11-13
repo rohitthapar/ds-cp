52956cafff04e4417dfde73a6b5f4efec25bd694
Rohit Thapar <thaprt206@gmail.com>
2023-11-13 18:39:07 +0530

    add: files

--- cses/Graphs/flightDiscount.cpp
+++ cses/Graphs/flightDiscount.cpp
@@ -1,37 +1,53 @@
-// #include <bits/stdc++.h>
-// #define pr pair<int, int>
-// using namespace std;
-// int main(){
-//     int n, m;
-//     cin >> n >> m;
-//     vector<vector<pr>> edges[n+1];
-//     for(int i=0;i<m;i++){
-//         int u, v, w;
-//         cin >> u >> v >> w;
-//         edges[u].push_back({v, wt});
-//     }
-//     int start = 1;
-//     priority_queue<pr, vector<pr>, greater<pr>> pq; // location, wt
-//     pq.push({start, 0}); 
-//     while(!pq.empty()){
-//         auto top = pq.top();
-//         pq.pop();
-//         int u = top.first;
-//         int wt = top.second;
-        
-
-//     }
-//     return 0;
-// }
-
-#include <iostream>
 #include <bits/stdc++.h>
+#define ll long long int
+#define pr pair<ll, ll>
 using namespace std;
-int main()
-{
-static float a=3.2;
-if(floor(a) + --a)(
-cout<<a;
+int main(){
+    ll n, m; cin >> n >> m;
+    vector<vector<pr>> gp(n+1);
+    // ll mx = ll_MIN, res = 0;
+    ll source = 1;
+    priority_queue<pair<ll, pr>, vector<pair<ll, pr>>, greater<pair<ll, pr>>> pq;
+    vector<ll> disTo(n+1, LONG_MAX);  // without coupon
+    vector<ll> wdDisTo(n+1, LONG_MAX);  // with coupon
 
-return 0;}
+    for(ll i=0;i<m;i++){
+        ll a, b, c;
+        cin >> a >> b >> c;
+        gp[a].push_back({b, c}); // from, to , cost
+    }
 
+    disTo[source] = wdDisTo[source] = 0;
+    pq.push({0, {source, 0}}); // distance, place, flag
+    while(!pq.empty()){
+        ll dis = pq.top().first;
+        ll src = pq.top().second.first;
+        ll flag = pq.top().second.second;
+        pq.pop();
+        if(flag == 1) if(wdDisTo[src] < dis) continue;
+        if(flag == 0) if(disTo[src] < dis) continue;
+        for(auto it : gp[src]){
+            ll des = it.first;
+            ll cost = it.second;
+            if(flag == 0){
+                if(disTo[des] > cost + dis){
+                    disTo[des] = cost + dis;
+                    pq.push({disTo[des], {des, 0}});
+                }
+                if(wdDisTo[des] > (floor(cost/2) + dis)){
+                    wdDisTo[des] = (floor(cost/2) + dis);
+                    pq.push({wdDisTo[des] , {des, 1}});
+                }
+            }
+            if(flag == 1){
+                if(wdDisTo[des] > cost + dis){
+                    wdDisTo[des] = cost + dis;
+                    pq.push({wdDisTo[des], {des, 1}});
+                }
+            }
+        }
+    }
+    cout << wdDisTo[n] << " ";
+    // for(auto d : disTo) cout << d << " ";
+    return 0;
+}
